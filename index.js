a25f2c59a431481b51bc9fe366f5a5c7dc4dc676
LALOBv2001 <79590204+LALOBv2001@users.noreply.github.com>
2023-02-15 08:56:37 -0600

    misFrases

---
+++ index.js
@@ -0,0 +1,16 @@
+const frases=[
+    "«Sé el cambio que quieres ver en el mundo» —Mahatma Gandhi",
+    "«No se trata de si van a derribarte, se trata de si vas a levantarte cuando lo hagan» —Vince Lombardi, entrenador de fúbol americano",
+    "«Nadie puede hacerte sentir inferior sin tu consentimiento» —Eleanor Roosevelt",
+    "«Qué maravilloso es que nadie tenga que esperar ni un segundo para empezar a mejorar el mundo» —Ana Frank",
+    "«El pesimista ve dificultades en cada oportunidad. El optimista ve oportunidades en cada dificultad» —Winston Churchill",
+    "«Muchos piensan en cambiar el mundo, pero casi nadie piensa en cambiarse a sí mismo» —Leon Tolstoi",
+    "«Si estás trabajando en algo que te importa de verdad, nadie tiene que empujarte: tu visión te empuja» —Steve Jobs"
+]
+function obtenerFrase(){
+    let indice=Math.floor(Math.random()*7)
+    return frases[indice]
+}
+console.log(obtenerFrase());
+
+module.exports.obtenerFrase=obtenerFrase();
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Frases Motivacionales 
+## Frases realizadas para poder consultarlas con Javascript
+### Este es una breve idea de lo que se hace para que nos pueda traer la informacion del mismo javascript 
+ ```Javascript
+function obtenerFrase(){
+    let indice=Math.floor(Math.random()*9)
+    return frases[indice]
+}
+```
+
